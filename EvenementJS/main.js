1167531a550d8feebdfbff34f66b9de5edeab7f8
Julien <julienmartin42120@gmail.com>
2021-02-24 22:12:23 +0100

    Exercices JS

---
+++ EvenementJS/main.js
@@ -0,0 +1,76 @@
+
+function random(number) {
+  return Math.floor(Math.random()*(number+1));
+}
+
+/* BOX1 */
+
+let box1 = document.querySelector("#box1")
+box1.addEventListener("click", function(event) {
+  let rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
+  box1.style.backgroundColor=rndCol;
+})
+
+/* BOX2 */
+
+let box2 = document.querySelector("#box2")
+box2.addEventListener("click", function(event) {
+  let rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
+  box2.style.backgroundColor=rndCol;
+})
+
+/* BOX3 */
+
+let box3 = document.querySelector("#box3")
+box3.addEventListener("click", function(event) {
+  let rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
+  box3.style.backgroundColor=rndCol;
+})
+
+/* BOX4 */
+
+let box4 = document.querySelector("#box4")
+box4.addEventListener("click", function(event) {
+  let rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
+  box4.style.backgroundColor=rndCol;
+})
+
+/* BOX5 */
+
+let box5 = document.querySelector("#box5")
+box5.addEventListener("click", function(event) {
+  let rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
+  box5.style.backgroundColor=rndCol;
+})
+
+/* BOX6 */
+
+let box6 = document.querySelector("#box6")
+box6.addEventListener("click", function(event) {
+  let rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
+  box6.style.backgroundColor=rndCol;
+})
+
+/* BOX7 */
+
+let box7 = document.querySelector("#box7")
+box7.addEventListener("click", function(event) {
+  let rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
+  box7.style.backgroundColor=rndCol;
+})
+
+/* BOX8 */
+
+let box8 = document.querySelector("#box8")
+box8.addEventListener("click", function(event) {
+  let rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
+  box8.style.backgroundColor=rndCol;
+})
+
+/* BOX9 */
+
+let box9 = document.querySelector("#box9")
+box9.addEventListener("click", function(event) {
+  let rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
+  box9.style.backgroundColor=rndCol;
+})
